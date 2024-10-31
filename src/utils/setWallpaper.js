5e8b6e51d9930247a82f9b65cf5fda0385c84b43
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2024-10-31 13:46:24 +0200

    add package

--- src/utils/setWallpaper.js
+++ src/utils/setWallpaper.js
@@ -1,5 +1,6 @@
 import * as FileSystem from 'expo-file-system';
 import * as MediaLibrary from 'expo-media-library';
+import * as Sharing from 'expo-sharing';
 
 export const setWallpaper = async (imageUri) => {
   console.log('image', imageUri);
@@ -14,10 +15,16 @@ export const setWallpaper = async (imageUri) => {
     const fileUri = `${FileSystem.cacheDirectory}wallpaper.jpg`;
     await FileSystem.downloadAsync(imageUri, fileUri);
 
-    const mediaAsset = await MediaLibrary.createAssetAsync(fileUri);
+    if (await Sharing.isAvailableAsync()) {
+      await Sharing.shareAsync(fileUri, {
+        dialogTitle: 'Установить обои',
+        UTI: 'public.image', 
+      });
+    } else {
+      console.log('Sharing is not available on this platform');
+    }
 
-    await MediaLibrary.createAlbumAsync('Wallpapers', mediaAsset, false);
-    console.log('Обои установлены');
+    console.log('Обои успешно установлены');
   } catch (error) {
     console.log('Ошибка при установке обоев:', error);
   }
